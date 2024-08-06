ec509c5b31fa65d7c0119674e053e69996ea40ce
VasilinaPlet <pletuhinavasilina@gmail.com>
2024-08-06 16:12:23 +0700

    No comment

---
+++ Practics/Условный оператор/task 9/Solution.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+sum = 0
+sum1 = 0
+sum2 = 0
+if 0 < a:
+    sum += a
+if 0 < b:
+    sum1 += b
+if 0 < c:
+    sum2 += c
+print(sum + sum1 + sum2)
